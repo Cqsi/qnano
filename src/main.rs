805fe64b5276d3cf1ccc1c34ac453b6ab4090de6
Casimir Rönnlöf <casimirr04@gmail.com>
2025-12-18 22:21:00 +0200

    working instruction parsing

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ use std::fs;
 fn main() {
     
     let filename = "code.qnano"
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file.");
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file.");
 
-    println!("With text:\n{contents}")
+    println!("With text:\n{contents}\n");
+
+    let mut instructions = parse_program(&contents);
+    
 }
 
 enum Gate {
@@ -19,7 +22,18 @@ fn parse_program(contents: &str) -> Vec<Gate> {
     let mut instructions: Vec<Gate> = Vec::new();
 
     for line in contents.lines() {
-        
+        let tokens: Vec<&str> = line.split_whitespace().collect();
+        if tokens.is_empty() { 
+            continue; 
+        }
+
+        match tokens[0] {
+            "h" => instructions.push(Gate::H(tokens[1].parse().unwrap())),
+            "x" => instructions.push(Gate::X(tokens[1].parse().unwrap())),
+            "cx" => instructions.push(Gate::CX(tokens[1].parse().unwrap(), tokens[2].parse().unwrap())),
+            _ => print!("Unknown gate: {}", tokens[0]),
+        }
     }
 
+    return instructions;
 }
